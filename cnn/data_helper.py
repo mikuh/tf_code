93b6bd4d9aa57263aabe134a6d905c9b617170a0
fengyanglu <853934146@qq.com>
2017-11-09 12:09:00 +0800

    添加InceptionV3 图片编码器

--- cnn/data_helper.py
+++ cnn/data_helper.py
@@ -139,7 +139,6 @@ def get_label2name_meta():
         label2name = pickle.load(f)
     return label2name
 
-
 def transfer_inputs(filename, batch_size, num_preprocess_threads, min_queue_examples):
     """迁移数据的输入
     """
@@ -164,6 +163,36 @@ def transfer_inputs(filename, batch_size, num_preprocess_threads, min_queue_exam
         min_after_dequeue=min_queue_examples)
 
     return image_batch, label_batch
+
+
+class InceptionModel(object):
+    def __init__(self, graph, input_layer_name="DecodeJpeg/contents:0", output_layer_name="pool_3:0"):
+        self.load_graph(graph)
+        with tf.Session() as sess:
+            self.sess = sess
+            self.input_layer_name = input_layer_name
+            self.image_tensor = sess.graph.get_tensor_by_name(output_layer_name)
+
+
+    def load_image(self, filename):
+        """读取图片"""
+        return tf.gfile.FastGFile(filename, 'rb').read()
+
+    def load_graph(self, filename):
+        """加载模型"""
+        with tf.gfile.FastGFile(filename, 'rb') as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+            tf.import_graph_def(graph_def, name='')
+
+    def encode_images(self, image):
+        """图片编码"""
+        image_data = self.load_image(image)
+        img_tensor = self.sess.run(self.image_tensor, {self.input_layer_name: image_data})
+        return img_tensor[0][0][0]
+
+
+
 
 
 if __name__ == '__main__':
